c00732ce6be572a7c7fa6bce19df02053a72b96b
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-11-15 18:04:32 +1300

    add deps and simple hello world

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # smallWorld
 Starting small to flesh out what's fun, useful and/or necessary re 3D worlds on the web
+
---
+++ src/deps.js
@@ -0,0 +1,2 @@
+export * from "https://cdn.esm.sh/v57/rollup@2.60.0/es2021/rollup.js";
+export * from "https://cdn.esm.sh/v57/svelte@3.44.1/es2021/svelte.js";
